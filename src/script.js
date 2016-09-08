1a9bb483c915ba6e9fc021de14034efb30d297ff
Guilherme Prezzi <guilhermeprezzi1997@gmail.com>
2016-09-08 18:22:51 -0300

    Scroll animado, closes #5.

--- src/script.js
+++ src/script.js
@@ -1,77 +1,91 @@
 (function ($) {
 
-	function calculatePosition(element) {
-		var walker = $('#walk-wrapper');
+    function calculatePosition(element) {
+        var walker = $('#walk-wrapper');
 
-		var size = element.outerHeight() > element.outerWidth() ? element.outerHeight() : element.outerWidth();
-		console.log('walk size:' + size);
+        var size = element.outerHeight() > element.outerWidth() ? element.outerHeight() : element.outerWidth();
+        console.log('walk size:' + size);
 
-		var position = element.offset();
-		console.log('walk position:' + position.left + 'x' + position.top);
+        var position = element.offset();
+        console.log('walk position:' + position.left + 'x' + position.top);
 
-		walker.css({
-			'height': (size + 20) + 'px',
-			'width': (size + 20) + 'px',
+        walker.css({
+            'height': (size + 20) + 'px',
+            'width': (size + 20) + 'px',
 
-			'margin-left': -((size + 20) / 2) + 'px',
-			'margin-top': -((size + 20) / 2) + 'px',
+            'margin-left': -((size + 20) / 2) + 'px',
+            'margin-top': -((size + 20) / 2) + 'px',
 
-			'left': (position.left + (element.outerWidth() / 2)) + 'px',
-			'top': (position.top + (element.outerHeight() / 2)) + 'px',
-		});
-	};
+            'left': (position.left + (element.outerWidth() / 2)) + 'px',
+            'top': (position.top + (element.outerHeight() / 2)) + 'px',
+        });
 
-		function calculateTextPosition(element) {
-		var size = element.outerHeight() > element.outerWidth() ? element.outerHeight() : element.outerWidth();
-		var position = element.offset();
-		var content = $('#walk-content');
+        //window.scrollTo(0, element.scrollTop());
 
-		var canRenderInRight = position.left + size + 20 + content.outerWidth() < $(window).width();
-		var canRenderInLeft = size + 20 + content.outerWidth() < $(window).width() / 2;
+        $('html, body').animate({
+            scrollTop: element.scrollTop()
+        }, 750);
+        setTimeout(function(){
+            walker.show();
+        }, 750);
+    
+    };
 
-		var canRenderInBottom = position.top + size + 20 + content.outerHeight() < $(window).height();
-		var canRenderInTop = size + 20 + content.outerHeight() < $(window).height() / 2;
+    function calculateTextPosition(element) {
+        var size = element.outerHeight() > element.outerWidth() ? element.outerHeight() : element.outerWidth();
+        var position = element.offset();
+        var content = $('#walk-content');
 
-		console.log('canRenderInRight: ' + canRenderInRight);
-		console.log('canRenderInLeft: ' + canRenderInLeft);
+        var canRenderInRight = position.left + size + 20 + content.outerWidth() < $(window).width();
+        var canRenderInLeft = size + 20 + content.outerWidth() < $(window).width() / 2;
 
-		console.log('canRenderInBottom: ' + canRenderInBottom);
-		console.log('canRenderInTop: ' + canRenderInTop);
+        var canRenderInBottom = position.top + size + 20 + content.outerHeight() < $(window).height();
+        var canRenderInTop = size + 20 + content.outerHeight() < $(window).height() / 2;
 
-		if (canRenderInRight || canRenderInLeft) {
-			$('#walk-content').css({
-				'left': canRenderInRight ? '100%' : '-300%',
-				'top': canRenderInBottom ? '100%' : '-180%',
-				'text-align': canRenderInRight ? 'left' : 'right'
-			});
-		} else {
-			// FICA NO CENTRO HORIZONTAL
-			$('#walk-content').css({
-				'left': '-200%',
-				'top': canRenderInBottom ? '100%' : '-180%',
-				'text-align': 'left'
-			});
-		}
-	}
+        console.log('canRenderInRight: ' + canRenderInRight);
+        console.log('canRenderInLeft: ' + canRenderInLeft);
 
-	function setupHandlers(element) {
-		$(window).on('resize', function () {
-			calculatePosition(element);
-			calculateTextPosition (element);
-		});
+        console.log('canRenderInBottom: ' + canRenderInBottom);
+        console.log('canRenderInTop: ' + canRenderInTop);
 
-		$('#walk-button').on('click',function(){
-			$('#walk-wrapper').hide();
-            $('html').css({'overflow': 'auto'});
-		});
-	};
+        if (canRenderInRight || canRenderInLeft) {
+            $('#walk-content').css({
+                'left': canRenderInRight ? '100%' : '-300%',
+                'top': canRenderInBottom ? '100%' : '-180%',
+                'text-align': canRenderInRight ? 'left' : 'right'
+            });
+        } else {
+            // FICA NO CENTRO HORIZONTAL
+            $('#walk-content').css({
+                'left': '-200%',
+                'top': canRenderInBottom ? '100%' : '-180%',
+                'text-align': 'left'
+            });
+        }
+    }
 
-	$.fn.walk = function (contentText, color) {
-		if(!this.width()) return; //Tentei .isEmptyObject() mas ele sempre retornava falso
-        $('html').css({'overflow': 'hidden'});
-		var element = this;
-		if ($('#walk-wrapper').length == 0) {
-			$('body').append(`
+    function setupHandlers(element) {
+        $(window).on('resize', function () {
+            calculatePosition(element);
+            calculateTextPosition(element);
+        });
+
+        $('#walk-button').on('click', function () {
+            $('#walk-wrapper').hide();
+            $('html').css({
+                'overflow': 'auto'
+            });
+        });
+    };
+
+    $.fn.walk = function (contentText, color) {
+        if (!this.width()) return; //Tentei .isEmptyObject() mas ele sempre retornava falso
+        $('html').css({
+            'overflow': 'hidden'
+        });
+        var element = this;
+        if ($('#walk-wrapper').length == 0) {
+            $('body').append(`
 	<div id="walk-wrapper">
         <div id="walk-content">
             <div id="walk-text">
@@ -80,18 +94,17 @@
         </div>
     </div>
 	`);
-		}
-		var walker = $('#walk-wrapper');
-		walker.show();
-		walker.css({
-			'border-color': (!!color) ? color : '#2196F3'
-		});
-        
+        }
+        var walker = $('#walk-wrapper');
+        walker.css({
+            'border-color': (!!color) ? color : '#2196F3'
+        });
+
         var walker_text = $('#walk-text');
         walker_text.html(contentText);
-                
-		calculatePosition(element);
-		calculateTextPosition(element);
-		setupHandlers(element);
-	};
+
+        calculatePosition(element);
+        calculateTextPosition(element);
+        setupHandlers(element);
+    };
 })(jQuery);
