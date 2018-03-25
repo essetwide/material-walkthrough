2b79d2ce29c497f64322432439bd7ee61f020ad9
Andy Wood <andy@ideaflip.com>
2018-03-25 10:41:47 +0100

    Use getBoundingClientRect to locate element more accurately, but don't forget to take into account the scrollY position for the top.

--- src/MaterialWalkthrough.js
+++ src/MaterialWalkthrough.js
@@ -414,9 +414,10 @@ export default class MaterialWalkthrough {
    * @private
    */
   static _renderFrame(target, renderCallback) {
-    const position = MaterialWalkthrough._position(target);
-    // Using this line.
-    const { height, width } = target.getClientRects()[0];
+    // Use the client bounding rect that includes css translation etc.
+    const { height, width, left, top } = target.getBoundingClientRect();
+    // Adjust the top to be relative to the document
+    const docTop = top + window.scrollY;
 
     let holeSize = height > width ? height : width; // Catch the biggest measure
     // Adjust with default min measure if it not higher than it
@@ -430,8 +431,8 @@ export default class MaterialWalkthrough {
       marginLeft: -((holeSize + MaterialWalkthrough.GUTTER) / 2) + 'px',
       marginTop: -((holeSize + MaterialWalkthrough.GUTTER) / 2) + 'px',
 
-      left: (position.left + (width / 2)) + 'px',
-      top: (position.top + (height / 2)) + 'px',
+      left: (left + (width / 2)) + 'px',
+      top: (docTop + (height / 2)) + 'px',
     };
     dom.setStyle(MaterialWalkthrough._wrapper, positions);
     _log('WALK_LOCK', 'Positioning \n' + JSON.stringify(positions, 2));
